30f997fbe147d7c202b8848875607f37b0f0c8b7
A. Belard <angebelard@Gmail.com>
2022-02-04 12:56:19 +0200

    Update MysqlConnectorDemo.java

--- src/mysql_connector_demo/MysqlConnectorDemo.java
+++ src/mysql_connector_demo/MysqlConnectorDemo.java
@@ -1,12 +1,10 @@
 package mysql_connector_demo;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class MysqlConnectorDemo {
     static String driver = "com.mysql.jdbc.Driver";
-    static String url = "jdbc:mysql://localhost:1306/demo";
+    static String url = "jdbc:mysql://localhost:3306/demo";
     static String username = "root";
     static String password = "";
     public static void main(String[] args) throws SQLException {
@@ -19,6 +17,32 @@ public class MysqlConnectorDemo {
                 System.exit(0);
             }
             System.out.println("Successfully connected..");
+            Statement statement;
+            try {
+                statement = conn.createStatement();
+                statement.execute("CREATE TABLE IF NOT EXISTS person(id INT PRIMARY KEY AUTO_INCREMENT," +
+                                                            "first_name VARCHAR(250), " +
+                                                            "last_name VARCHAR(250)," +
+                                                            "gender VARCHAR(250))");
+//                statement.execute(
+//                        "INSERT INTO person(first_name, last_name, gender)" +
+//                                "VALUES('Ange Belard', 'RUTAYISIRE', 'Male')"
+//                );
+                ResultSet rs = statement.executeQuery(
+                        "SELECT * FROM person"
+                );
+                while(rs.next()){
+                    System.out.println(rs.getInt(1) + " " + rs.getString("first_name") + " " + rs.getString(3) + " " + rs.getString(4));
+                }
+                String tableName = rs.getMetaData().getTableName(1);
+                String db = rs.getMetaData().getCatalogName(1);
+                for(int i = 1; i < rs.getMetaData().getColumnCount(); i++){
+                    System.out.println(rs.getMetaData().getColumnName(i));
+                }
+            }
+            catch (Exception e){
+                System.out.println(e.getMessage());
+            }
             conn.close();
         } catch (Exception e) {
             System.out.println("Connection failed!");
